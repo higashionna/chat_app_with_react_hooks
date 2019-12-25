27aa26a3aacc28ef40d9a85bdc3ff4f65bab2b5a
shichida <m.shichida@trecon.co.jp>
2019-12-25 22:55:55 +0900

    localstorageからログイン情報を取ってくるのを変更

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,3 @@
 export { ADD_CHAT_ROOM } from './chatRooms';
-export { ADD_CURRENT_USER_INFO, DELETE_CURRENT_USER_INFO, SET_CURRENT_USER_INFO_FROM_LOCALSTORAGE } from './currentUserInfos';
+export { ADD_CURRENT_USER_INFO, DELETE_CURRENT_USER_INFO, SET_CURRENT_USER_INFO } from './currentUserInfos';
 export { ADD_MESSAGE, SET_MESSAGES } from './messages';
